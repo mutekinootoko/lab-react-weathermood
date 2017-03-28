5fe1ae0206ef5d9516a5ca549776e200682c81d8
shwu <brandon.shwu@gmail.com>
2017-03-28 17:08:46 +0800

    Init commit.

---
+++ src/components/WeatherDisplay.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './WeatherDisplay.css';
+
+export default class WeatherDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className={`weather-display ${this.props.masking
+                ? 'masking'
+                : ''}`}>
+                <img src={`images/w-${this.props.group}.png`}/>
+                <p className='description'>{this.props.description}</p>&nbsp;
+                <h1 className='temp'>
+                    <span className='display-3'>{this.props.temp.toFixed(0)}&ordm;</span>
+                    &nbsp;{(this.props.unit === 'metric')
+                        ? 'C'
+                        : 'F'}
+                </h1>
+            </div>
+        );
+    }
+}
